e087cd02657c327748eba319cea48ebc18fb3375
Volodymyr Ponomarenko <volodymyr.ponomarenko.js@gmail.com>
2020-01-28 10:29:46 +0200

    CSS with JS

---
+++ project1/shared.js
@@ -0,0 +1,40 @@
+var backdrop = document.querySelector('.backdrop');
+var selectPlanButtons = document.querySelectorAll('.plans button');
+var modal = document.querySelector('.modal');
+var modalNoButton = document.querySelector(".modal__action--negative");
+var toggleButton = document.querySelector('.toggle-button');
+var mobile = document.querySelector('.mobile-nav');
+for (var i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener('click', function () {
+    /*modal.style.display = 'block';
+    backdrop.style.display = 'block';*/
+    modal.classList.add('open');
+    backdrop.classList.add('open');
+
+  })
+}
+backdrop.addEventListener('click', function () {
+  /*mobile.style.display = 'none';*/
+  mobile.classList.remove('open');
+  closeModal();
+})
+if (modalNoButton) {
+  modalNoButton.addEventListener('click', closeModal);
+}
+function closeModal() {
+  /*backdrop.style.display = "none";
+  modal.style.display = "none";*/
+  if (modal) {
+    modal.classList.remove('open');
+  }
+
+  backdrop.classList.remove('open');
+}
+toggleButton.addEventListener('click', function () {
+  /*mobile.style.display = 'block';
+  backdrop.style.display = 'block';*/
+  mobile.classList.add('open');
+  backdrop.classList.add('open');
+}
+)
+
